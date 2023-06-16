c804864d1ffd69ebacb3c9d61ff14b303076c994
Fabiano Arthur Passos Correa De Oliveira <Fabiano.Correa@sicoob.com.br>
2023-06-16 16:48:48 -0300

    adicionei test.txt e calc.java

--- calc.java
+++ calc.java
@@ -1,1 +1,53 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    int i = 0;
+	    
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+		System.out.print("login\tsenha (123)");
+		
+		System.out.print("\ndigite a senha:");
+		int senha = sc.nextInt();
+		if(senha == 123){
 
+	        while (i!=5){
+		    System.out.println("\tCalculadora\nescolha o que deseja fazer:\n(1)adição\t(2)Subtração\n(3)divisão\t(4)multiplicação\n(5)encerrar");
+	        i = sc.nextInt();
+	        if(i==1){
+	            System.out.print("digite os valores a serem somados separadamente: ");
+	            float a1 = sc.nextFloat();
+	            float a2 = sc.nextFloat();
+	            System.out.printf("A soma é: %f\n",a1+a2);
+	        }
+	        else if (i==2){
+	            System.out.print("digite os valores a serem subtraidos separadamente: ");
+	            float a1 = sc.nextFloat();
+	            float a2 = sc.nextFloat();
+	            System.out.printf("A soma é: %f\n",a1-a2);
+	        } else if (i==3){
+	            System.out.print("digite os valores a serem divididos separadamente: ");
+	            float a1 = sc.nextFloat();
+	            float a2 = sc.nextFloat();
+	            System.out.printf("A soma é: %f\n",a1/a2);
+	        } else if (i==4){
+	            System.out.print("digite os valores a serem mutiplicas separadamente: ");
+	            float a1 = sc.nextFloat();
+	            float a2 = sc.nextFloat();
+	            System.out.printf("A soma é: %f\n",a1*a2);
+	        }else if (i==5){
+	            System.out.print("obrigado por usar a calucladora :D");
+	        } 
+	        else{
+	            System.out.print("digite uma opção valida");
+	        }
+
+
+	   }
+	}else{
+		System.out.print("senha ou usuariso invalidos");
+	}
+	}
+}
